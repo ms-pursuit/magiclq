652f1e7888b912975bdfa382bbf82cabbf862e1d
Msahana36 <128680292+Msahana36@users.noreply.github.com>
2024-03-15 17:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,26 +189,29 @@ for msg in st.session_state.messages:
         st.chat_message("user", avatar=user_image).write(msg.content)
     memory.chat_memory.add_message(msg)
     
+st.cache_resource(ttl="2h")   
+def chatbot():
+    if prompt := st.chat_input(placeholder=starter_message):
+        st.chat_message("user", avatar=user_image).write(prompt)
+        with st.chat_message("assistant", avatar=avatar_image):
+            with st.spinner("Thinking..."):
+                st_callback = StreamlitCallbackHandler(st.container())
+                response = agent_executor(
+                    {"input": prompt, "history": st.session_state.messages},
+                    #callbacks=[st_callback],
+                    include_run_info=True,
+                )
+                st.session_state.messages.append(AIMessage(content=response["output"]))
+                st.write_stream(stream_data(response["output"]))
+                if os.path.exists('graph.png'):  
+                    st.image("graph.png")   
+                memory.save_context({"input": prompt}, response)
+                if os.path.exists('graph.png'):
+                    os.remove('graph.png')
+                st.session_state["messages"] = memory.buffer
+                run_id = response["__run"].run_id
 
-if prompt := st.chat_input(placeholder=starter_message):
-    st.chat_message("user", avatar=user_image).write(prompt)
-    with st.chat_message("assistant", avatar=avatar_image):
-        with st.spinner("Thinking..."):
-            st_callback = StreamlitCallbackHandler(st.container())
-            response = agent_executor(
-                {"input": prompt, "history": st.session_state.messages},
-                #callbacks=[st_callback],
-                include_run_info=True,
-            )
-            st.session_state.messages.append(AIMessage(content=response["output"]))
-            st.write_stream(stream_data(response["output"]))
-            if os.path.exists('graph.png'):  
-                st.image("graph.png")   
-            memory.save_context({"input": prompt}, response)
-            if os.path.exists('graph.png'):
-                os.remove('graph.png')
-            st.session_state["messages"] = memory.buffer
-            run_id = response["__run"].run_id
+chatbot()
         
         
         
